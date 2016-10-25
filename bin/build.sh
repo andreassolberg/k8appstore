16a3d424e4b1de38d404008f2c63a71cd6bb84a6
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-10-25 17:06:46 +0200

    Build updates deploymentconfig

--- bin/build.sh
+++ bin/build.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
-VERSION="1.0.8"
+VERSION="1.1.3"
 IMAGE="uninettno/k8appstore:$VERSION"
 
 echo "Building $IMAGE"
+echo "{\"version\": \"$VERSION\"}" > app/etc/version.json
 
 # docker build -t andreassolberg/smedstua -t gcr.io/solberg-cluster/ .
 docker build -t $IMAGE .
-#docker push $IMAGE
+docker push $IMAGE
 #gcloud docker push $IMAGE
 
+
+jq ".spec.template.spec.containers[0].image = \"$IMAGE\"" etc-kubectl/appstore-deployment.json > etc-kubectl/appstore-deployment-update.json
+
+
 echo "Image ready $IMAGE"
-echo "kubectl --context daas --namespace appenginecore replace -f etc-kubectl/appstore-deployment.json"
-#kubectl --context daas --namespace appenginecore replace -f etc-kubectl/appstore-deployment.json
+echo "kubectl --context daas --namespace appenginecore replace -f etc-kubectl/appstore-deployment-update.json"
+kubectl --context daas --namespace appenginecore replace -f etc-kubectl/appstore-deployment-update.json
